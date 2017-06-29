615d514104b41374fb69232b2e1e100373a16ab6
Robert Kelevra <kelevra.robert@gmail.com>
2017-06-29 10:15:20 +0200

    Add files via upload

--- validation/receptive.py
+++ validation/receptive.py
@@ -4,13 +4,47 @@
 # all rights go to Dushyant Mehta where i found this sript on stack overflow
 # https://stackoverflow.com/questions/35582521/how-to-calculate-receptive-field-size
 
-convnet =[[11,4,0],[3,2,0],[5,1,2],[3,2,0],[3,1,1],[3,1,1],[3,1,1],[3,2,0],[6,1,0]]
-convnet =[[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1]]
-convnet =[[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[3,1,1]]
-layer_name = ['conv1','pool1','conv2','pool2','conv3','conv4','conv5','pool5','fc6-conv']
-layer_name = ['conv1','pool1','conv2','pool2','conv3','pool3','conv4','pool4','conv5','pool5','conv6','pool6','conv7','conv8']
+#FUTURE YOLO IMPLEMENTATION
+# convnet =[[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1]]
+# layer_name = ['conv1','pool1','conv2','pool2','conv3','pool3','conv4','pool4','conv5','pool5','conv6','pool6','conv7','conv8']
+# imsize = 448
 
-imsize = 448
+#original with images that have size 128*76 87.63%
+# convnet =[[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0]]
+# layer_name = ['conv1','conv1','pool1','conv2','conv2','pool2','conv3','conv3','pool3']
+# imsize = 128
+
+#prototype1
+convnet =[[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1]]
+layer_name = ['conv1','conv1','conv1','pool1','conv2','conv2','conv2','pool2','conv3','conv3','conv3','pool3','recept1']
+imsize = 128
+
+#prototype1.2
+# convnet =[[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1]]
+# layer_name = ['conv1','conv1','pool1','conv2','conv2','pool2','conv3','conv3','conv3','pool3','recept1']
+# imsize = 128
+
+#prototype2
+# convnet =[[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0]]
+# layer_name = ['conv1','conv1','conv1','conv1','pool1','conv2','conv2','conv2','conv2','pool2','conv3','conv3','conv3','conv3','pool3']
+# imsize = 128
+
+#EXTREME PROTOTYPE small images
+# convnet =[[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0]]
+# layer_name = ['conv1','conv1','conv1','conv1','conv1','conv1','pool1','conv2','conv2','conv2','conv2','conv2','conv2','pool2','conv3','conv3','conv3','conv3','conv3','conv3','pool3']
+# imsize = 128
+
+# large images
+# convnet =[[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0]]
+# layer_name = ['conv1','conv1','pool1','conv2','conv2','pool2','conv3','conv3','pool3','conv4','conv4','pool4']
+# imsize = 256
+
+#EXTREME PROTOTYPE large images
+# convnet =[[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[3,1,1],[2,2,0],[3,1,1],[3,1,1],[2,2,0]]
+# layer_name = ['conv1','conv1','conv1','conv1','conv1','conv1','pool1','conv2','conv2','conv2','conv2','conv2','conv2','pool2','conv3','conv3','conv3','conv3','conv3','conv3','pool3','conv4','conv4','pool4']
+# imsize = 256
+
+
 
 def outFromIn(isz, layernum = 9, net = convnet):
     if layernum>len(net): layernum=len(net)
@@ -41,4 +75,4 @@ if __name__ == '__main__':
     for i in range(len(convnet)):
         p = outFromIn(imsize,i+1)
         rf = inFromOut(i+1)
-        print "Layer Name = %s, Output size = %3d, Stride = % 3d, RF size = %3d" % (layer_name[i], p[0], p[1], rf)
+        print "Layer Name = %s, Output size = %3d, Stride = % 3d, RF size = %3dx%d" % (layer_name[i], p[0], p[1], rf,rf)
